e29cc962ba7187fb6e40b0dd84240876e48582ff
Barman <dbarman2@dxc.com>
2019-11-22 21:07:19 +0530

    Client Side Login & Error Hnadling Completed

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { ValueComponent } from "./value/value.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AuthService } from "./_services/auth.service";
+
+import { RegisterComponent } from "./components/register/register.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
